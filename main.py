5940ea445400edf89eafbebf85cb29e77d492642
Connie Deng Deng <98533897+conniedeng303@users.noreply.github.com>
2022-02-07 21:31:47 +0000

    finished part A and B

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-
+import random
 #Part A
+classes_per_week = 3
+
 weeks = 16
 classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week)
 
+cost_per_class = (cost_per_week / classes_per_week)
+
+print("Cost per week:", cost_per_week)
+print("The cost per class:", cost_per_class, type (cost_per_class))
 
 #Part B
+my_list = ["orange", "lemon", "apple", "banana", "coconut"]
+print("The random fruit is",random.choice(my_list))
